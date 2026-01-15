36a1ab2e9f63964294a0f3ac8e0e552d5316dae4
Weronika Olejniczak <wer.olejniczak@gmail.com>
2026-01-15 20:52:09 +0100

    flashlight, forward+ and bubble particles

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,18 +2,23 @@ extends RigidBody3D
 class_name Player
 
 ## How much thrust force to apply when moving
-@export var thrust: float = 30.0
+@export var thrust: float = 100.0
 
 ## How much torque force to apply when rotating
-@export var torque_thrust: float = 20.0
+@export var torque_thrust: float = 25.0
 
 ## What is the max speed to clamp velocity
-@export var max_speed: float = 15.0
+@export var max_speed: float = 200.0
+
+@onready var bubble_particles: GPUParticles3D = $BubbleParticles
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:	
 	if (Input.is_action_pressed("boost")):
+		bubble_particles.emitting = true
 		apply_central_force(-basis.y * delta * thrust)
+	else:
+		bubble_particles.emitting = false
 		
 	if (Input.is_action_pressed("rotate_left")):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
