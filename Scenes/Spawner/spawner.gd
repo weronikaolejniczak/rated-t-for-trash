9c0b9f683e947bfe402eeecb8db042eab9282a28
Weronika Olejniczak <wer.olejniczak@gmail.com>
2026-01-16 20:22:44 +0100

    background music

--- Scenes/Spawner/spawner.gd
+++ Scenes/Spawner/spawner.gd
@@ -5,22 +5,18 @@ extends Node3D
 @export var speed: float = 5.0
 @export var left_x: float = -10.0
 @export var right_x: float = 10.0
-@export var min_y = 1.0
-@export var max_y = 5.0
-
-@onready var camera_3d: Camera3D = $"../Camera/Camera3D"
+@export var up_y = 4.0
+@export var down_y = -4.0
 
 var sides: Array[String] = ["left", "right"]
-var timer: float = 0.0
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	timer += delta
-	
-	if (timer >= spawn_interval):
-		timer = 0
-		spawn_object()
 
 func spawn_object():
 	var side = sides.pick_random()
-	# var object = object_to_spawn.instantiate()
+	var object = object_to_spawn.instantiate()
+	var position = Vector3(0.0, 0.0, 0.0)
+	
+	object.position = position
+	add_child(object)
+
+func _on_timer_timeout() -> void:
+	spawn_object()
