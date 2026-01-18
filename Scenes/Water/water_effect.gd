894f9d8b2690bdfcb7cb112b35d7021f1db7ce37
Weronika Olejniczak <wer.olejniczak@gmail.com>
2026-01-18 19:40:33 +0100

    make water texture fill screen

--- Scenes/Water/water_effect.gd
+++ Scenes/Water/water_effect.gd
@@ -1,10 +1,11 @@
-extends Node2D
+extends MarginContainer
+
 
 @export var player: Player
 
 @onready var mat: ShaderMaterial = $Texture.material
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(_delta: float) -> void:
 	if (mat and player):
 		var pos = Vector2(player.global_position.x, player.global_position.y)
