b5805f90db91143610292748fb4843ca62bb675d
Weronika Olejniczak <wer.olejniczak@gmail.com>
2026-01-18 01:05:33 +0100

    redo movement to WASD

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -3,34 +3,50 @@ class_name Player
 
 ## How much thrust force to apply when moving
 @export var thrust: float = 100.0
-## How much torque force to apply when rotating
-@export var torque_thrust: float = 25.0
+## How much torque strength to apply when rotating
+@export var torque_strength: float = 25.0
 ## What is the max speed to clamp velocity
 @export var max_speed: float = 200.0
+## How much the mesh should tilt in radians
+@export var tilt_angle: float = deg_to_rad(30.0)
+## How fast should the robot tilt
+@export var tilt_speed: float = 6.0
+## What is the initial rotation of the mesh
+@export var initial_rotation: Vector3 = Vector3(0.0, deg_to_rad(-90.0), 0.0)
 
 @onready var robot_sfx: FmodEventEmitter2D = $RobotSFX
-@onready var bubble_particles: GPUParticles3D = $BubbleParticles
+@onready var robot_mesh: Node3D = $Robot
+@onready var bubble_particles: GPUParticles3D = $Robot/BubbleParticles
 
 func get_depth() -> int:
 	var depth = floor(self.global_position.y)
 	return depth
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	var move_input = Vector3.ZERO
+	var target_tilt = initial_rotation
 	
-	if (Input.is_action_pressed("boost")):
-		apply_central_force(-basis.y * delta * thrust)
+	if Input.is_action_pressed("move_up"):
+		move_input.y += 1
+		target_tilt.z = tilt_angle
+	if Input.is_action_pressed("move_down"):
+		move_input.y -= 1
+		target_tilt.z = -tilt_angle
+	if Input.is_action_pressed("move_left"):
+		move_input.x -= 1
+		target_tilt.y = initial_rotation.y - tilt_angle
+	if Input.is_action_pressed("move_right"):
+		move_input.x += 1
+		target_tilt.y = initial_rotation.y + tilt_angle
+	
+	if move_input != Vector3.ZERO:
+		apply_central_force(move_input.normalized() * thrust * delta)
 		bubble_particles.emitting = true
 		robot_sfx.set_parameter("states", "swimming")
 	else:
 		bubble_particles.emitting = false
 		robot_sfx.set_parameter("states", "idle")
 	
-	if (Input.is_action_pressed("rotate_left")):
-		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
-	
-	if (Input.is_action_pressed("rotate_right")):
-		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
-	
-	if (linear_velocity.length() > max_speed):
-		linear_velocity = linear_velocity.normalized() * max_speed
+	robot_mesh.rotation.x = lerp(robot_mesh.rotation.x, target_tilt.x, delta * tilt_speed)
+	robot_mesh.rotation.y = lerp(robot_mesh.rotation.y, target_tilt.y, delta * tilt_speed)
+	robot_mesh.rotation.z = lerp(robot_mesh.rotation.z, target_tilt.z, delta * tilt_speed)
